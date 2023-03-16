49125428c3549be013409d541f6ab2f11bb2e971
SgtMarmite <d.novotny31415@gmail.com>
2023-03-16 20:35:40 +0100

    uploading fixed

--- src/component.py
+++ src/component.py
@@ -5,6 +5,7 @@ from datetime import datetime, timedelta
 import re
 
 import msal
+import requests.exceptions
 from O365 import Account, FileSystemTokenBackend
 
 from keboola.component.base import ComponentBase
@@ -62,7 +63,7 @@ class Component(ComponentBase):
     def get_date_of_processing(date):
         dt_str_1 = date
         dt_str_2 = "today"
-        dt_format = "%Y-%m-%d"
+        dt_format = "%Y_%m_%d"
         date_of_processing, _ = parse_datetime_interval(dt_str_1, dt_str_2, dt_format)
         return str(date_of_processing)
 
@@ -72,9 +73,9 @@ class Component(ComponentBase):
 
     @staticmethod
     def subtract_one_day(date_string):
-        date_obj = datetime.strptime(date_string, '%Y-%m-%d')
+        date_obj = datetime.strptime(date_string, '%Y_%m_%d')
         new_date = date_obj - timedelta(days=1)
-        return new_date.strftime('%Y-%m-%d')
+        return new_date.strftime('%Y_%m_%d')
 
     @staticmethod
     def extract_date(string):
@@ -91,34 +92,47 @@ class Component(ComponentBase):
         logging.info(f"Processing date: {date_of_processing}")
 
         folder = main_folder_path + date_of_processing
-        if not folder.startswith("/"):
-            folder = "/" + folder
 
         if operation_type == "upload":
             files = self.get_input_files()
             for file in files:
                 filename_date = self.extract_date(file.name)
                 if self.subtract_one_day(date_of_processing) == filename_date:
-                    self.upload(folder_name=folder, file_name=file.name)
+                    self.upload(folder_name=folder, file=file)
         elif operation_type == "download":
             self.download(folder_name=folder)
         else:
             raise UserException(f"Invalid operation type: {operation_type}")
 
-    def upload(self, folder_name, file_name):
+    def upload(self, folder_name, file):
         """Uploads file_name Sharepoint folder_name (with '/')"""
-        folder = self.sharepoint_drive.get_item_by_path(folder_name)
-        input_file_path = os.path.join(self.files_in_path, file_name)
-        logging.info(f"Uploading file: {file_name}")
-
-        # Create OneDrive folder if it does not exist
         try:
+            folder = self.sharepoint_drive.get_item_by_path(folder_name)
+        except requests.exceptions.HTTPError:
+            logging.info(f"Folder {folder_name} does not exist. The component will attempt to create it.")
             logging.info(f"Trying to create new folder: {folder_name}")
-            folder.create_child_folder(folder_name)
-        except Exception as e:
-            logging.error(f"Cannot create new folder {folder_name}, exception: {e}")
+            folder = self.sharepoint_drive.get_root_folder()
+            self.create_new_onedrive_folder(folder, folder_name)
+            folder = self.sharepoint_drive.get_item_by_path(folder_name)
+
+        input_file_path = os.path.join(file.full_path)
+        logging.info(f"Uploading file: {file.name}")
 
         folder.upload_file(item=input_file_path)
+
+    def create_new_onedrive_folder(self, folder, path):
+        path = path[1:]
+        path_list = path.split("/")  # Split the string into a list using "/"
+        current_path = ""
+        for item in path_list:
+            try:
+                folder.create_child_folder(item)
+                logging.info(f"Subfolder {item} created.")
+            except requests.exceptions.HTTPError:
+                logging.info(f"Subfolder {item} already exists.")
+            finally:
+                current_path += f"/{item}"
+                folder = self.sharepoint_drive.get_item_by_path(current_path)
 
     def download(self, folder_name):
         """Downloads all files in a specified OneDrive folder."""
